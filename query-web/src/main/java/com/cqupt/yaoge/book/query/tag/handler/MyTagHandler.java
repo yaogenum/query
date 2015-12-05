e00759bea7324998f77139123df5bb1a8986c2c5
yaogenum <yaogemail@gmail.com>
2015-12-05 16:23:26 +0800

    update

--- query-web/src/main/java/com/cqupt/yaoge/book/query/tag/handler/MyTagHandler.java
+++ query-web/src/main/java/com/cqupt/yaoge/book/query/tag/handler/MyTagHandler.java
@@ -1,12 +1,14 @@
 package com.cqupt.yaoge.book.query.tag.handler;
+
 import com.cqupt.yaoge.book.query.tag.parser.MyTagParser;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
 /**
  * Created by yaoge on 15/11/17.
  */
-public class MyTagHandler extends NamespaceHandlerSupport{
+public class MyTagHandler extends NamespaceHandlerSupport {
+
     public void init() {
-        registerBeanDefinitionParser("format",
-                new MyTagParser());
+        registerBeanDefinitionParser("format", new MyTagParser());
     }
 }
